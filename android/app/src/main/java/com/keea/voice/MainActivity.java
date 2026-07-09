872e750f62b96e6740a78bfc372c36aa08931e81
Benedict Ackon <ackonbright158@gmail.com>
2026-07-09 04:29:12 +0000

    Configure Capacitor and add Android APK GitHub Action pipeline

---
+++ android/app/src/main/java/com/keea/voice/MainActivity.java
@@ -0,0 +1,5 @@
+package com.keea.voice;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.keea.voice',
+  appName: 'KEEA NPP Voice',
+  webDir: 'dist'
+};
+
+export default config;
